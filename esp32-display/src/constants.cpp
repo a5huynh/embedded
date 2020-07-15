81d13237e3838d2f05389e0063ccabba45fae8df
Andrew Huynh <a5thuynh@gmail.com>
2020-07-15 12:01:42 -0700

    ✨ [esp32-display] Add blank constants.cpp

---
+++ esp32-display/src/constants.cpp
@@ -0,0 +1,13 @@
+#include "constants.h"
+
+// API used for the COVID stats tracking
+const char* COVID_API = "https://covidtracking.com/api/us";
+
+// WIFI config
+const char* WIFI_SSID = "YOUR_SSID";
+const char* WIFI_PASS = "YOUR_PASSWORD";
+
+// UI constants
+const int LINE_PADDING = 8;
+const int X_PADDING = 16;
+const int Y_PADDING = 16;
